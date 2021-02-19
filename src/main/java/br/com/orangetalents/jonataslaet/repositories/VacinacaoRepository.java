817974b9493e2e666dd4415c60b92cda0a361356
Jonatas Laet <jonataslaetprogramador@gmail.com>
2021-02-19 01:49:56 -0300

    Mudadas as interfaces repository para o pacote repositories

--- src/main/java/br/com/orangetalents/jonataslaet/repositories/VacinacaoRepository.java
+++ src/main/java/br/com/orangetalents/jonataslaet/repositories/VacinacaoRepository.java
@@ -1,4 +1,4 @@
-package br.com.orangetalents.jonataslaet;
+package br.com.orangetalents.jonataslaet.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
