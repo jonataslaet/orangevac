5c0a247c44e85527476e93b5464258620ea8fd5e
Jonatas Laet <jonataslaetprogramador@gmail.com>
2021-02-19 00:23:10 -0300

    Criado o projeto implementadas primeira e segunda etapas selecionadas as dependências, e definidas as propriedades de conexão com o banco de dados

---
+++ src/main/java/br/com/orangetalents/jonataslaet/OrangevacApplication.java
@@ -0,0 +1,13 @@
+package br.com.orangetalents.jonataslaet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrangevacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrangevacApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/orangetalents/jonataslaet/OrangevacApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.orangetalents.jonataslaet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrangevacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
